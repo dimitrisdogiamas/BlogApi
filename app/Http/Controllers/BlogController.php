0cea404fa1f8071fdb4c61a245947373370c51b1
dimitrisdogiamas <112798108+dimitrisdogiamas@users.noreply.github.com>
2025-06-21 17:23:36 +0300

    blog api functioning

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -25,4 +25,44 @@ class BlogController extends Controller
         return response()->json($post, 201);
     }
 
+    // function to show a single post 
+    public function show($id) {
+        $post = Post::find($id);
+
+        // if the post does not exist, return a 404 error
+        if(!$post) {
+            return response()->json(['message' => 'Post not found'], 404);
+        }
+
+        return response()->json($post);
+    }
+
+    public function update(Request $request, $id) {
+        $post = Post::find($id);
+
+        if(!$post) {
+            return response()->json(['message' => "Post not found"], 404);
+        }
+//   we update the post data via request 
+
+        $post->update([
+            'title' => $request->input('title'),
+            'content'=> $request->input('content'),
+        ]);
+
+        return response()->json($post);
+    }
+
+    public function destroy($id) {
+        $post = Post::find($id);
+
+        if(!$post) {
+            return response()->json(['message' => "Post not found"], 404);
+        }
+        // the delete method
+
+        $post->delete();
+
+        return response()->json(['message' => "Post deleted successfully"], 201);
+    }
 }
--- routes/blog.php
+++ routes/blog.php
@@ -1,6 +1,11 @@
+<?php
+
 use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/posts', [BlogController::class, 'index']);
 Route::post('/posts', [BlogController::class, 'store']);
+Route::get('/posts/{id}', [BlogController::class, 'show']);
+Route::put('/posts/{id}', [BlogController::class, 'update']);
+Route::delete('/posts/{id}', [BlogController::class, 'destroy']);
